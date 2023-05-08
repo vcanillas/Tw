eb918462451011ee8b6780a4c3523802be93b270
VI <viopera@outlook.com>
2023-05-08 18:45:10 +0200

    rm_solo

--- src/pages/api/matchup.ts
+++ src/pages/api/matchup.ts
@@ -34,19 +34,17 @@ async function matchup(id: string) {
       let win: number = 0, loss: number = 0;
 
       data.games.filter(is_rmsolo_and_notcurrent).forEach(function (game: any, idx: number, arr: any) {
-        if (!game.ongoing) {
-          game.teams.forEach(function (team: any, idx2: number, arr2: any) {
-            if (idx == 0) {
-              lastGame += `${ref_civ(team[0].player.civilization)} ${team[0].player.profile_id != id ? `[${team[0].player.name}]` : ""}`
-              if (idx2 == 0) { lastGame += " vs "; }
-            }
+        game.teams.forEach(function (team: any, idx2: number, arr2: any) {
+          if (idx == 0) {
+            lastGame += `${ref_civ(team[0].player.civilization)} ${team[0].player.profile_id != id ? `[${team[0].player.name}]` : ""}`
+            if (idx2 == 0) { lastGame += " vs "; }
+          }
 
-            if (team[0].player.profile_id == id) {
-              if (team[0].player.result == "win") { win = win + 1; }
-              if (team[0].player.result == "loss") { loss = loss + 1; }
-            }
-          });
-        }
+          if (team[0].player.profile_id == id) {
+            if (team[0].player.result == "win") { win = win + 1; }
+            if (team[0].player.result == "loss") { loss = loss + 1; }
+          }
+        });
       });
 
       result += `Résultat contre ${nameOpponent} - ${win} ${win > 1 ? "victoires" : "victoire"} / ${loss} ${loss > 1 ? "défaites" : "défaite"}. `
@@ -61,7 +59,7 @@ async function matchup(id: string) {
 }
 
 export async function getServerSideProps(id: string, idOpponent: string) {
-  return await aoe4worldConnector(`https://aoe4world.com/api/v0/players/${id}/games?opponent_profile_id=${idOpponent}`)
+  return await aoe4worldConnector(`https://aoe4world.com/api/v0/players/${id}/games?opponent_profile_id=${idOpponent}&leaderboard=rm_solo`)
 }
 
 export async function getServerSidePropsGame(id: string) {
